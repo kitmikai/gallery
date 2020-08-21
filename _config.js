b1ef8d9e523294aadc598bc95f27794441bbbdc3
tondieki <tondieki@safaricom.co.ke>
2020-08-21 23:56:46 +0300

    Change connection credentials to Atlas Mongodb

--- _config.js
+++ _config.js
@@ -1,9 +1,8 @@
 var config = {}
 
-// Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production = "mongodb+srv://moorings:tech-xir-R43@cluster0.lltah.mongodb.net/darkroom?retryWrites=true&w=majority",
+    development = "mongodb+srv://moorings:tech-xir-R43@cluster0.lltah.mongodb.net/darkroom-dev?retryWrites=true&w=majority",
+    test = "mongodb+srv://moorings:tech-xir-R43@cluster0.lltah.mongodb.net/darkroom-test?retryWrites=true&w=majority",
 }
 module.exports = config;
