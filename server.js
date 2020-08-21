d242612e3967364907539ba6b400a184bd0152ba
tondieki <tondieki@safaricom.co.ke>
2020-08-22 02:26:27 +0300

    Correct PORT and app initialization

--- server.js
+++ server.js
@@ -4,19 +4,12 @@ const mongoose = require("mongoose");
 const path = require("path");
 const config = require("./_config");
 
-// Initializing the app
-const app = express();
-
 // Define routes
 let index = require("./routes/index");
 let image = require("./routes/image");
 
-// connecting the database
-// let mongodb_url = 'mongodb://localhost:27017/';
-// let dbName = 'darkroom';
-// mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-//     if (err) console.log(err)
-// });
+// Initializing the app
+const app = express();
 
 const MONGODB_URI =
   process.env.MONGODB_URI || config.MONGODB_URI[app.settings.env];
@@ -32,12 +25,6 @@ mongoose.connect(
   }
 );
 
-// test if the database has connected successfully
-let db = mongoose.connection;
-db.once("open", () => {
-  console.log("Database connected successfully");
-});
-
 // View Engine
 app.set("view engine", "ejs");
 
@@ -50,7 +37,7 @@ app.use(express.json());
 app.use("/", index);
 app.use("/image", image);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
