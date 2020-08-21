f6682afaaf6bf95c20d53c94e1273bcfc6fda173
tondieki <tondieki@safaricom.co.ke>
2020-08-22 01:33:27 +0300

    Replace double quotes with single quotes

--- _config.js
+++ _config.js
@@ -1,8 +1,8 @@
 var config = {}
 
 config.mongoURI = {
-    production = 'mongodb+srv://moorings:tech-xir-R43@cluster0.lltah.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development = 'mongodb+srv://moorings:tech-xir-R43@cluster0.lltah.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test = 'mongodb+srv://moorings:tech-xir-R43@cluster0.lltah.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production = 'mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development = 'mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test = 'mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
