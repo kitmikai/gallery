e031970f7d4d725da1f96401f70955c8a378661b
tondieki <tondieki@safaricom.co.ke>
2020-08-22 19:58:00 +0300

    Edit credentials

--- _config.js
+++ _config.js
@@ -1,8 +1,12 @@
-var config = {}
+var config = {};
 
+// Update to have your correct username and password
 config.mongoURI = {
-    production = 'mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development = 'mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test = 'mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+  production:
+    "mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom?retryWrites=true&w=majority",
+  development:
+    "mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom-dev?retryWrites=true&w=majority",
+  test:
+    "mongodb+srv://photoman:alc-at3-P1x@cluster0.lltah.mongodb.net/darkroom-test?retryWrites=true&w=majority",
+};
 module.exports = config;
