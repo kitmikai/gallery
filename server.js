53275f226c0cb909599dfa9ccb4fa12e237dc4ae
tondieki <tondieki@safaricom.co.ke>
2020-08-22 11:29:52 +0300

    Edit PORT

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use("/", index);
 app.use("/image", image);
 
-//const port = process.env.PORT || 5000;
-app.listen(process.env.PORT || 5000, function () {
-  console.log("Server is listening at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening at http://localhost:${PORT}`);
 });
