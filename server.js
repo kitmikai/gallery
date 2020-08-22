601058e6dd2d7a44192ddb63817a39e7d2a565e0
tondieki <tondieki@safaricom.co.ke>
2020-08-23 00:21:02 +0300

    Accept merge changes

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -8,39 +7,25 @@ const config = require("./_config");
 // Define routes
 let index = require("./routes/index");
 let image = require("./routes/image");
-=======
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const config = require('./_config');
-
-// Define routes
-let index = require('./routes/index');
-let image = require('./routes/image');
 
 // Initializing the app
 const app = express();
 
 // connecting the database
 
-const MONGODB_URI = process.env.MONGODB_URI || config.mongoURI[app.settings.env]
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true  },(err)=>{
+const MONGODB_URI =
+  process.env.MONGODB_URI || config.mongoURI[app.settings.env];
+mongoose.connect(
+  MONGODB_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
     if (err) {
-        console.log(err)
-    }else{
-        console.log(`Connected to Database: ${MONGODB_URI}`)
+      console.log(err);
+    } else {
+      console.log(`Connected to Database: ${MONGODB_URI}`);
     }
-});
-
-// test if the database has connected successfully
-// let db = mongoose.connection;
-// db.once('open', ()=>{
-//     console.log('Database connected successfully')
-// })
-
->>>>>>> test
-
+  }
+);
 
 const MONGODB_URI =
   process.env.MONGODB_URI || config.mongoURI[app.settings.env];
@@ -69,15 +54,6 @@ app.use("/", index);
 app.use("/image", image);
 
 const PORT = process.env.PORT || 5000;
-<<<<<<< HEAD
 app.listen(PORT, () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
 });
-=======
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
-});
-
-
-module.exports = app;
->>>>>>> test
